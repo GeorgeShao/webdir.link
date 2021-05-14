b1c983b64988e2846eb7fea2b69e76f2047baca8
GeorgeShao <GeorgeShao123@gmail.com>
2021-05-13 21:54:40 -0400

    ⚡ Setup DynamoDB url data retrieval

--- src/pages/Shortener.js
+++ src/pages/Shortener.js
@@ -3,9 +3,24 @@ import { Box, Center, Flex, Grid, Spacer, Text, Button, InputGroup, Input, Input
 import { ArrowUpIcon } from '@chakra-ui/icons'
 import { Formik, Field, Form } from "formik";
 import ThemeToggle from "../components/ThemeToggle"
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { listShortenedLinkPairs } from '../graphql/queries';
 
 function Shortener() {
+  const [urls, setURLs] = useState([]);
   const [messageBody, setMessageBody] = useState('');
+
+  useEffect(() => {
+    API
+      .graphql(graphqlOperation(listShortenedLinkPairs))
+      .then((response) => {
+        const items = response.data?.listShortenedLinkPairs?.items;
+        
+        if (items) {
+          setURLs(items);
+        }
+      });
+  }, []);
 
   const handleChange = (event) => {
     setMessageBody(event.target.value);
@@ -29,39 +44,42 @@ function Shortener() {
   };
 
   return (
-      <div>
-        <Grid templateColumns="repeat(3, 1fr)">
-          <Flex>
-            <Box>
-              <Text fontSize="3xl" marginLeft="4" color="#815ad5">webdir.link</Text>
-              <Text fontSize="md" marginLeft="4" color="#815ad5">URL Shortener</Text>
-            </Box>
-            <Spacer />
-          </Flex>
-            <Box marginTop="2">
-            <Formik>
-              {(props) => (
-                <Form onSubmit={handleSubmit}>
-                  <Field name="message">
-                    {({ field, form }) => (
-                      <InputGroup size="md">
-                        <Input {...field} name="message" placeholder="Enter Message..." onChange={handleChange} value={messageBody}/>
-                        <InputRightElement>
-                          <IconButton h="1.75rem" size="md" marginEnd="10px" icon={<ArrowUpIcon/>} colorScheme="purple" type="submit" onSubmit={handleSubmit}/>
-                        </InputRightElement>
-                      </InputGroup>
-                    )}
-                  </Field>
-                </Form>
-              )}
-            </Formik>
+    <div>
+      <Grid templateColumns="repeat(3, 1fr)">
+        <Flex>
+          <Box>
+            <Text fontSize="3xl" marginLeft="4" color="#815ad5">webdir.link</Text>
+            <Text fontSize="md" marginLeft="4" color="#815ad5">URL Shortener</Text>
           </Box>
-          <Flex>
-            <Spacer />
-            <ThemeToggle />
-          </Flex>
-        </Grid>
-      </div>
+          {urls.map((url) => (
+            <div key={url.id}>{url.customURL} --> {url.targetURL}</div>
+          ))}
+          <Spacer />
+        </Flex>
+          <Box marginTop="2">
+          <Formik>
+            {(props) => (
+              <Form onSubmit={handleSubmit}>
+                <Field name="message">
+                  {({ field, form }) => (
+                    <InputGroup size="md">
+                      <Input {...field} name="message" placeholder="Enter Message..." onChange={handleChange} value={messageBody}/>
+                      <InputRightElement>
+                        <IconButton h="1.75rem" size="md" marginEnd="10px" icon={<ArrowUpIcon/>} colorScheme="purple" type="submit" onSubmit={handleSubmit}/>
+                      </InputRightElement>
+                    </InputGroup>
+                  )}
+                </Field>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+        <Flex>
+          <Spacer />
+          <ThemeToggle />
+        </Flex>
+      </Grid>
+    </div>
   )
 }
 
